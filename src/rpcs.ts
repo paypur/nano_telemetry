69f61b69d8a8278dc17a53b69983a69f7554556f
paypur <pieceofpaypur@gmail.com>
2024-03-15 19:09:38 -0400

    samples offline nodes that are already in the db

--- src/rpcs.ts
+++ src/rpcs.ts
@@ -1,5 +1,7 @@
 import 'dotenv/config'
 
+// https://docs.nano.org/commands/rpc-protocol/
+
 export async function getNodeWeights() {
     const result = await fetch(process.env.NODE_RPC!, {
         method: "POST",
@@ -10,4 +12,16 @@ export async function getNodeWeights() {
     })
     const data = await result.json()
     return data.representatives
+}
+
+export async function getAccountWeight(nanoAddress: string): Promise<string> {
+    const result = await fetch(process.env.NODE_RPC!, {
+        method: "POST",
+        body: JSON.stringify({
+            "action": "account_weight",
+            "account": nanoAddress
+        }),
+    })
+    const data = await result.json()
+    return data.weight
 }
